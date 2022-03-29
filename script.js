47a9d0f74b3dd5251f3e7cf48ae664725999d7fc
JOSH <onyemajoshua892@gmail.com>
2022-03-29 12:49:59 +0100

    save

---
+++ script.js
@@ -0,0 +1,16 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// const x = 23;
+
+// const calcAge = birthYear => 2037 - birthYear;
+// console.log(calcAge(1991));
+
+// Problem:
+// We wor for a company buiding a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that cometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+
+// 2) Breaking up into sub-problems
